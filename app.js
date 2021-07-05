1b0ff58bb24215417fa0231299eb9bb9014bdd7f
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-07-05 22:00:31 +0530

    Added Local storage functionality

--- app.js
+++ app.js
@@ -27,7 +27,9 @@ class UI {
       <td><a href='#' class="delete">X</a></td>` //this delete class will be lated used to delete the book from table
       
     //append this row as a child to the table list
-    list.appendChild(row)  
+    list.appendChild(row)
+    //add the book to local storage as well
+    localStore.pushNewBookToStorage(book)  
   }
 
   //Delete book from the List UI
@@ -69,6 +71,64 @@ class UI {
   }
 }
 
+//Storage class to store the Books in Local Storage
+class localStore {
+  //get the books array stored in the local storage
+  static getBooksFromStorage() {
+    //create an array that will store the books fetched from the local storage
+    let booksArray;
+    //check if local storage has the books item
+    if(localStorage.getItem('books') === null) {
+      //If there is no item found then booksArray will be an empty array
+      booksArray = []
+    } else {
+      //if there is an item books found in local storage then parse and save it in booksArray
+      booksArray = JSON.parse(localStorage.getItem('books'))
+    }
+    return booksArray
+  }
+
+  //display the books when the application is reloaded or started
+  static displayBooksFromStorage() {
+    //get the books from the local storage
+    const booksArray = localStore.getBooksFromStorage()
+    //using forEach loop to loop through the books array and push the books one by one to the UI
+    booksArray.forEach((book) => {
+      const ui = new UI()
+      ui.addBookToList(book)
+    })
+  }
+
+  //Add new book to local storage when book if added to table list in the UI
+  static pushNewBookToStorage(book) {
+    //get the books item from local storage
+    const booksArray = localStore.getBooksFromStorage()
+    //add this new book to the books array
+    booksArray.push(book)
+    //push this new booksArray back to the local storage
+    localStorage.setItem('books', JSON.stringify(booksArray))
+  }
+
+  //delete book from local storage when it is deleted from the table list UI
+  //We use the isbn as the reference here to match the book with a book having the same isbn in booksArray and delete it
+  static deleteBookFromStorage(isbn) {
+    //get the books item from the local storage
+    const booksArray = localStore.getBooksFromStorage()
+    //using forEach loop to delete the book that has the matching isbn
+    booksArray.forEach((book,index) => {
+      if(book.isbn === isbn) {
+        booksArray.splice(index,1)
+      }
+    })
+    
+    //push the modified books Array back to the local Storage
+    localStorage.setItem('books', JSON.stringify(booksArray))
+  }
+}
+
+//DOM event to load all books from local storage and push them to table list on UI
+document.addEventListener('DOMContentLoaded', localStore.displayBooksFromStorage())
+
 //Event listener for form submit button 
 //The form accepts three inputs i.e. title, author and isbn
 document.querySelector('#book-form').addEventListener('submit', (e) => {
@@ -106,6 +166,11 @@ document.querySelector('#book-list').addEventListener('click', (e) => {
   const ui = new UI()
   //function that deletes the book from the UI
   ui.deleteBookFromList(e)
+  //delete the book from the local storage
+  //here previousElementSibling is the column td which contains isbn
+  //We only get the isbn number by using textContent
+  localStore.deleteBookFromStorage(e.target.parentElement.previousElementSibling.textContent) 
   //show custom alert saying that the book was deleted from the UI
   ui.showCustomAlert('Book removed from list','success')
+  e.preventDefault()
 })
