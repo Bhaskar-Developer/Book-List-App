471f654325f1ef32894acf5d03c08f0799dbaecd
Bhaskar-Developer <bhaskarchetty22@gmail.com>
2021-07-05 15:57:40 +0530

    Added logic to add the book to the table UI

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ class Book {
 //Adding a new Book
 //Deleting a book
 class UI {
+  //Add book to the Table list UI
+  addBookToList(book) {
+    console.log(book)
+    //select the table list
+    const list = document.getElementById('book-list')
+
+    //create a new row element to push to table
+    const row = document.createElement('tr')
+
+    //Add the title,author and isbn as the row data along with a link that will be used to remove a book
+    row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href='#' class="delete">X</a><td>` //this delete class will be lated used to delete the book from table
+      
+    //append this row as a child to the table list
+    list.appendChild(row)  
+  }
   //clear Form Fields
   clearFormFields() {
     document.getElementById('title').value = ''
@@ -39,7 +58,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const ui = new UI();
 
     //Add the book to table on UI
-    
+    ui.addBookToList(book)
     //clear all Form fields
     ui.clearFormFields()
   }
